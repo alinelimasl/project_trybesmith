63de8edc2d58f29a210793db5e496862ac36c789
Aline <aline_lima@outlook.com>
2024-01-17 14:46:33 -0300

    Corrige testes

--- tests/unit/services/products.service.test.ts
+++ tests/unit/services/products.service.test.ts
@@ -5,32 +5,31 @@ import productMock  from '../../mocks/product.mock';
 import productService from '../../../src/services/product.service';
 
 describe('ProductsService', function () {
-  beforeEach(function () { sinon.restore(); });
-  it('Deve retornar com status 201 com o produto cadastrado'), async function () {
-//arrange
-
-const productMockReturn = ProductModel.build(productMock.validProduct);
-sinon.stub(ProductModel, 'create').resolves(productMockReturn);
-
-//act
-
-const serviceResponse = await productService.create(productMock.validProduct);
-
-//assert
-expect(serviceResponse.status).to.be.equal('SUCCESSFUL');
-expect(serviceResponse.data).to.be.deep.equal(productMock.validProduct);
-}
-it('Deve retornar com status 400 sem o nome corretamente'), async function () {
-//arrange
-const productMockReturn = ProductModel.build(productMock.emptyNameProduct);
-sinon.stub(ProductModel, 'create').resolves(productMockReturn);
-
-//act
-const serviceResponse = await productService.create(productMock.emptyNameProduct);
-
-//assert
-expect(serviceResponse.status).to.be.equal('INVALID_DATA');
-expect(serviceResponse.data).to.be.deep.equal({ message: 'Nome inválido' });
-}
-
+  beforeEach(function () { 
+    sinon.restore(); 
+    it('Deve retornar com status 201 com o produto cadastrado', async function () {
+      // Arrange
+      const productMockReturn = ProductModel.build(productMock.validProduct);
+      sinon.stub(ProductModel, 'create').resolves(productMockReturn);
+  
+      // Act
+      const serviceResponse = await productService.create(productMock.validProduct);
+  
+      // Assert
+      expect(serviceResponse.status).to.be.equal('SUCCESSFUL');
+      expect(serviceResponse.data).to.be.deep.equal(productMock.validProduct);
+    });
+    it('Deve retornar com status 400 sem o nome corretamente', async function () {
+      // Arrange
+      const productMockReturn = ProductModel.build(productMock.emptyNameProduct);
+      sinon.stub(ProductModel, 'create').resolves(productMockReturn);
+  
+      // Act
+      const serviceResponse = await productService.create(productMock.emptyNameProduct);
+  
+      // Assert
+      expect(serviceResponse.status).to.be.equal('INVALID_DATA');
+      expect(serviceResponse.data).to.be.deep.equal({ message: 'Nome inválido' });
+    });
+  });
 });
