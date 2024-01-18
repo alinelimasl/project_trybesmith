2a70e5f337e4f4063de1f7b1e06890080a68cb94
Aline <aline_lima@outlook.com>
2024-01-18 10:52:44 -0300

    Requisito 5: validações

--- src/routers/product.route.ts
+++ src/routers/product.route.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import productController from '../controllers/product.controller';
+import auth from '../middlewares/auth.middleware';
 
 const productRouter = Router();
 
-productRouter.post('/products', productController.create);
+productRouter.post('/products', auth.nameProduct, auth.priceProduct, productController.create);
 productRouter.get('/products', productController.list);
 
 export default productRouter;
